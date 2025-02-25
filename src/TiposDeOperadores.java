aedd667bd97c5b04e106ebe985e5f9e3976b1d71
MadJhonny <joaogepe@gmail.com>
2025-02-25 10:52:49 -0300

    Calculadora básica, praticando operadores aritiméticos

--- src/TiposDeOperadores.java
+++ src/TiposDeOperadores.java
@@ -1,3 +1,49 @@
+import java.util.Scanner;
+
 public class TiposDeOperadores {
-    
+
+   public static void main (String args []) {
+
+    Scanner scanner = new Scanner (System.in);
+
+      System.out.print("\n---CALCULADORA BÁSICA---");
+
+      System.out.print("\n Insira o primeiro valor: ");
+      int primeiroNumero = scanner.nextInt();
+
+      System.out.print("\n Insira o segundo número: ");
+      int segundoNumero = scanner.nextInt();
+
+      scanner.nextLine();
+
+      System.out.print("\n Escolha um operador: + | - | * | /: ");
+      String operador = scanner.nextLine();
+ 
+      if (operador.equals("+")) {
+
+      System.out.println(primeiroNumero + segundoNumero);
+
+      }
+
+      else if (operador.equals("-")) {
+
+      System.out.println(primeiroNumero - segundoNumero);
+
+      }
+
+      else if (operador.equals("*")) {
+
+      System.out.println(primeiroNumero * segundoNumero);
+      
+      }
+
+      else {
+
+        System.out.println(primeiroNumero / segundoNumero);
+      }
+
+      scanner.close();
+ 
+   }
+
 }
